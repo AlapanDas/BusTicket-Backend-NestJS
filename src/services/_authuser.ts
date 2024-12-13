02faa8ce2dd53d4a673c1834bb5425a9cffdfda6
Alapan Das <71692310+AlapanDas@users.noreply.github.com>
2024-12-13 21:35:41 +0530

    Update _authuser.ts

--- src/services/_authuser.ts
+++ src/services/_authuser.ts
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 
 export class authUser {
      constructor(payload: userDetails) {
-          const secretKey = 'blackcat@69';
+          const secretKey = '{SECRET}';
           const options = { expiresIn: '23h' };
           const token = jwt.sign(payload, secretKey, options);
           return token
